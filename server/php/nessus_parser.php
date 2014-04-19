cf38036ccf77500f0a51b01dbbb05371b9b96b8e
Ben Finke <ben.finke@gmail.com>
2014-04-19 14:43:12 -0400

    Adding some spacing for before the title line

--- server/php/nessus_parser.php
+++ server/php/nessus_parser.php
@@ -37,6 +37,8 @@ function isXML( $file ) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <![endif]-->
 <meta charset="utf-8">
+<br>
+<br>
 <title>Cargo Plane</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Bootstrap styles -->
