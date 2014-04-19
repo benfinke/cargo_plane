e373625de23009355ab7347455370dcc6d0601a2
Ben Finke <ben.finke@gmail.com>
2014-04-19 14:15:13 -0400

    basic nessus_parser.php implementation in place, added a few additional directories

--- server/php/nessus_parser.php
+++ server/php/nessus_parser.php
@@ -11,5 +11,33 @@
  */
 
 error_reporting(E_ALL | E_STRICT);
-require('UploadHandler.php');
-$upload_handler = new UploadHandler();
+
+function isXML( $file ) {                
+    libxml_use_internal_errors(true); 
+    $doc = new DOMDocument('1.0', 'utf-8'); 
+    $doc->loadXML(file_get_contents($file)); 
+
+    $errors = libxml_get_errors(); 
+    if (empty($errors)) 
+    { 
+        return true; 
+    } 
+	else {
+		return false;
+	}
+}
+
+$files = scandir('files/');
+
+foreach($files as $file) {
+	if (isXML($file)==TRUE)
+	{
+		system("parser.py -i $file --csv nessus_csv/$file.csv");
+		move_uploaded_file($file, nessus_archive/$file);
+	}
+	else {
+		echo "There was a problem with the $file file.";
+	}
+}
+
+?>
