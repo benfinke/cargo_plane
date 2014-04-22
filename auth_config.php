49bab63519b43c6ab0ecf498fdb3952948cfe114
Ben Finke <ben.finke@gmail.com>
2014-04-22 06:18:54 -0400

    Added class defintion to auth_config.php

--- auth_config.php
+++ auth_config.php
@@ -1,5 +1,28 @@
 <?php
 
+
+
+class FGMembersite
+{
+    var $admin_email;
+    var $from_address;
+    
+    var $username;
+    var $pwd;
+    var $database;
+    var $tablename;
+    var $connection;
+    var $rand_key;
+    
+    var $error_message;
+    
+    //-----Initialization -------
+    function FGMembersite()
+    {
+        $this->sitename = 'scout.entint.com';
+        $this->rand_key = '0iQx5oBk66oVZep';
+    }
+
 function CheckLogin()
 {
      session_start();
@@ -13,4 +36,5 @@ function CheckLogin()
      return true;
 }
 
+}
 ?>
